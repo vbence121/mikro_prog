652bf4975e497d708b4aeee4e4408fe3fc2ad4a6
vbence121 <vbence121@gmail.com>
2019-09-21 16:38:02 +0200

    Initialization and basic led & main py file

---
+++ led.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+def blink ( pin ):
+    #this functiuon blinks the led connected to the passed through port
+    # -.-.---
+    GPIO.output(pin, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(pin, GPIO.LOW)
+    sleep(1)
+    GPIO.output(pin, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(pin, GPIO.LOW)
+    sleep(1)
+    GPIO.output(pin, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(pin, GPIO.LOW)
+    return
+def lighup (pin, t):
+    # turn on the given GPIO pin led, for given t time
+    GPIO.output(pin, GPIO.HIGH)
+    sleep(t)
+    GPIO.output(pin, GPIO.LOW)
---
+++ main.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GIO
+import sleep
+import time
+
+while True:
+    #if rfid detected
+    # if in datasheet
+    #   open gate && start enterance timer
+    #   adjust nmbr of spaces
+    #     send out msg to outer serv
