914099eed9de981fc007b675880e777914f9848d
vbence121 <vbence121@gmail.com>
2019-09-25 19:07:29 +0200

    talalka2

---
+++ led_functions.py
@@ -0,0 +1,29 @@
+#import RPi.GPIO as GPIO
+#from time import sleep
+
+def blink ( pin ):
+    #this functiuon blinks the led connected to the passed through port
+    # -.-.---
+  """
+    GPIO.output(pin, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(pin, GPIO.LOW)
+    sleep(1)
+    GPIO.output(pin, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(pin, GPIO.LOW)
+    sleep(1)
+    GPIO.output(pin, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(pin, GPIO.LOW)
+  """
+print(pin)
+return
+"""
+def lighup (pin, t):
+    # turn on the given GPIO pin led, for given t time
+    GPIO.output(pin, GPIO.HIGH)
+    sleep(t)
+    GPIO.output(pin, GPIO.LOW)
+  """
+
